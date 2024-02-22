fb65b5c062275250fceee995812476ac2f8dd503
Vladislav <vladislav.hushcha@gmail.com>
2024-02-22 20:14:06 +0300

    hamburger menu + fixes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    const menu_btn = document.querySelector('.header__menu-burger')
+    const mobile_menu = document.querySelector('.header__menu-list-mobile')
+
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+}
