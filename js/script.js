bd16762ba7fabe93fe3271940dd90cd8e6a424f3
Vladislav <vladislav.hushcha@gmail.com>
2024-02-23 13:18:24 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.onload = function () {
     const menu_btn = document.querySelector('.header__menu-burger')
     const mobile_menu = document.querySelector('.header__menu-list-mobile')
     const cover = document.querySelector('.header__menu-burger-cover');
+    var contact_btn = document.querySelector('.contact-btn');
 
     menu_btn.addEventListener('click', function(){
         menu_btn.classList.toggle('active');
